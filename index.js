627776f1dc2ac029493968a26b7b4492b2dffdde
nblancoz <nblancozappala@gmail.com>
2023-09-26 23:29:18 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let cargador = ["pium!, pium!, pium!, pium!, pium!, pium!, pium!"]
+function cauchat(cargador) {
+    for (let i = 0; i < cargador.length; i++) {
+        console.log(cargador[i]);   
+    }
+}
+// no se que más hacer
